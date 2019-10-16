5e6c4b6de542fbcdc6aa70b18a8c0cd154e0c118
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-10-17 00:45:10 +0800

    Add user database and notifications

--- src/views/database/users.vue
+++ src/views/database/users.vue
@@ -1,55 +1,55 @@
 <template>
   <v-container>
-    <v-ripple>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :single-expand="singleExpand"
-      :expanded.sync="expanded"
-      :search="search"
-      item-key="name"
-      show-expand
-      @click:row="clicked"
-    >
-      <template v-slot:top>
-        <v-toolbar flat>
-          <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-account-search"
-            label="Search"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-toolbar>
-      </template>
-      <template v-slot:expanded-item="{ headers }">
-        <td :colspan="headers.length">
-          <v-btn text>
-            View Permit Records
-          </v-btn>
-          <v-btn text>
-            Violations
-          </v-btn>
-          <v-btn text>
-            Accountabilities
-          </v-btn>
-          <v-btn text>
-            Update Profile
-          </v-btn>
-          <v-btn text>
-            Update PIS
-          </v-btn>
-          <v-btn color="red" text>
-            Reset Account
-          </v-btn>
-          <v-btn color="red" text>
-            Checkout
-          </v-btn>
-        </td>
-      </template>
-    </v-data-table>
-    </v-ripple>
+    <v-card>
+      <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :single-expand="singleExpand"
+        :expanded.sync="expanded"
+        :search="search"
+        item-key="name"
+        show-expand
+        @click:row="clicked"
+      >
+        <template v-slot:top>
+          <v-toolbar flat>
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="search"
+              append-icon="mdi-account-search"
+              label="Search"
+              single-line
+              hide-details
+            ></v-text-field>
+          </v-toolbar>
+        </template>
+        <template v-slot:expanded-item="{ headers }">
+          <td :colspan="headers.length">
+            <v-btn text>
+              View Permit Records
+            </v-btn>
+            <v-btn text>
+              Violations
+            </v-btn>
+            <v-btn text>
+              Accountabilities
+            </v-btn>
+            <v-btn text>
+              Update Profile
+            </v-btn>
+            <v-btn text>
+              Update PIS
+            </v-btn>
+            <v-btn color="red" text>
+              Reset Account
+            </v-btn>
+            <v-btn color="red" text>
+              Checkout
+            </v-btn>
+          </td>
+        </template>
+      </v-data-table>
+    </v-card>
   </v-container>
 </template>
 
