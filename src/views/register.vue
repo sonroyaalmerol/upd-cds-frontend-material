5c6ff8522c4c6ba2dd4c2546909ec5a3d8fecb03
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-10-26 21:42:35 +0800

    change auth colors to primary

--- src/views/register.vue
+++ src/views/register.vue
@@ -8,18 +8,18 @@
           </v-toolbar>
           <v-card-text>
             <v-form>
-              <v-text-field label="Student Number" name="upid" type="text" color="secondary"></v-text-field>
-              <v-text-field label="Email Address" name="email" type="text" color="secondary"></v-text-field>
-              <v-text-field label="Username" name="username" type="text" color="secondary"></v-text-field>
-              <v-text-field id="password" label="Password" name="password" type="password" color="secondary">
+              <v-text-field label="Student Number" name="upid" type="text" color="primary"></v-text-field>
+              <v-text-field label="Email Address" name="email" type="text" color="primary"></v-text-field>
+              <v-text-field label="Username" name="username" type="text" color="primary"></v-text-field>
+              <v-text-field id="password" label="Password" name="password" type="password" color="primary">
               </v-text-field>
               <v-text-field id="confirmpass" label="Confirm Password" name="confirmpass" type="password"
-                color="secondary"></v-text-field>
+                color="primary"></v-text-field>
             </v-form>
           </v-card-text>
           <v-card-actions>
             <div class="flex-grow-1"></div>
-            <v-btn color="secondary" x-large text @click.stop="$router.push('login')">Back to Login</v-btn>
+            <v-btn color="primary" x-large text @click.stop="$router.push('login')">Back to Login</v-btn>
             <v-btn color="primary" x-large>Register</v-btn>
           </v-card-actions>
         </v-card>
