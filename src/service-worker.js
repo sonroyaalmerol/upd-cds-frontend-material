ff0c8ffe1d0191ac20d87402541efa11a0960d18
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-10-27 02:53:42 +0800

    change service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,8 +3,6 @@ if (workbox) {
   // apply precaching. In the built version, the precacheManifest will
   // be imported using importScripts (as is workbox itself) and we can 
   // precache this. This is all we need for precaching
-  workbox.precaching.precacheAndRoute(self.__precacheManifest)
-
   workbox.routing.registerNavigationRoute('/index.html')
 
   workbox.routing.registerRoute(
