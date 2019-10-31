72084347bb9918ee22094db6a3e28063210bf515
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-10-31 21:20:53 +0800

    fix mobile view

--- src/components/database/ProfileForm.vue
+++ src/components/database/ProfileForm.vue
@@ -18,100 +18,100 @@
               </center>
             </v-col>
           </v-row>
-          <v-text-field rounded outlined v-model="residentForm.upid" :counter="10" label="Student Number" required></v-text-field>
-          <v-text-field rounded outlined v-model="residentForm.krhid" :counter="12" label="KRH ID Number" required></v-text-field>
-          <v-row>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.firstName" label="First Name" required></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.lastName" label="Last Name" required></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.college" label="College"></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.degree" label="Degree Program"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-switch color="primary" v-model="residentForm.isAthletePerformer" label="University Athlete/Performer">
-              </v-switch>
-            </v-col>
-            <v-col>
-              <v-switch color="primary" v-model="residentForm.isCouncil" label="House Council Member"></v-switch>
-            </v-col>
-          </v-row>
+          <v-text-field rounded outlined v-model="residentForm.upid" :counter="10" label="Student Number" required>
+          </v-text-field>
+          <v-text-field rounded outlined v-model="residentForm.krhid" :counter="12" label="KRH ID Number" required>
+          </v-text-field>
+          <template v-if="!isMobileDevice">
+            <v-row>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.firstName" label="First Name" required>
+                </v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.lastName" label="Last Name" required>
+                </v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.college" label="College"></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.degree" label="Degree Program"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-switch color="primary" v-model="residentForm.isAthletePerformer"
+                  label="University Athlete/Performer">
+                </v-switch>
+              </v-col>
+              <v-col>
+                <v-switch color="primary" v-model="residentForm.isCouncil" label="House Council Member"></v-switch>
+              </v-col>
+            </v-row>
+          </template>
+          <template v-else>
+            <v-text-field rounded outlined v-model="residentForm.firstName" label="First Name" required></v-text-field>
+            <v-text-field rounded outlined v-model="residentForm.lastName" label="Last Name" required></v-text-field>
+            <v-text-field rounded outlined v-model="residentForm.college" label="College"></v-text-field>
+            <v-text-field rounded outlined v-model="residentForm.degree" label="Degree Program"></v-text-field>
+            <v-switch color="primary" v-model="residentForm.isAthletePerformer" label="University Athlete/Performer">
+            </v-switch>
+            <v-switch color="primary" v-model="residentForm.isCouncil" label="House Council Member"></v-switch>
+          </template>
           <v-text-field rounded outlined v-model="residentForm.contact" label="Resident Contact Number"></v-text-field>
-          <v-text-field rounded outlined v-model="residentForm.emergency" label="Person to contact in case of emerency"></v-text-field>
-          <v-text-field rounded outlined v-model="residentForm.emergencyContact" label="Emergency Contact Number"></v-text-field>
+          <v-text-field rounded outlined v-model="residentForm.emergency" label="Person to contact in case of emerency">
+          </v-text-field>
+          <v-text-field rounded outlined v-model="residentForm.emergencyContact" label="Emergency Contact Number">
+          </v-text-field>
           <v-radio-group v-model="residentForm.sex" label="Sex" row>
             <v-radio color="blue" label="Male" value="Male"></v-radio>
             <v-radio color="red" label="Female" value="Female"></v-radio>
           </v-radio-group>
-          <v-row>
-            <v-col>
-              <v-select rounded outlined v-model="residentForm.corridor" :items="[{
-                                    text: 'Basement Boys',
-                                    value: 'BB'
-                                },
-                                {
-                                    text: '1st Floor Boys',
-                                    value: '1FB'
-                                },
-                                {
-                                    text: '2nd Floor Boys',
-                                    value: '2FB'
-                                },
-                                {
-                                    text: '3rd Floor Boys',
-                                    value: '3FB'
-                                },
-                                {
-                                    text: 'Centerfold Girls',
-                                    value: 'CF'
-                                },
-                                {
-                                    text: '1st Floor Girls',
-                                    value: '1FG'
-                                },
-                                {
-                                    text: '2nd Floor Girls',
-                                    value: '2FG'
-                                },
-                                {
-                                    text: '3rd Floor Girls',
-                                    value: '3FG'
-                                }]" label="Corridor"></v-select>
-            </v-col>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.room" label="Room Number"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.religion" label="Religion"></v-text-field>
-            </v-col>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.civilStatus" label="Civil Status"></v-text-field>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <DatePicker v-model="residentForm.birthday" label="Birthday" />
-            </v-col>
-            <v-col>
-              <v-text-field rounded outlined v-model="residentForm.stsBracket" label="STS Bracket"></v-text-field>
-            </v-col>
-          </v-row>
+          <template v-if="!isMobileDevice">
+            <v-row>
+              <v-col>
+                <v-select rounded outlined v-model="residentForm.corridor" :items="corridors" label="Corridor">
+                </v-select>
+              </v-col>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.room" label="Room Number"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.religion" label="Religion"></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.civilStatus" label="Civil Status"></v-text-field>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col>
+                <DatePicker v-model="residentForm.birthday" label="Birthday" />
+              </v-col>
+              <v-col>
+                <v-text-field rounded outlined v-model="residentForm.stsBracket" label="STS Bracket"></v-text-field>
+              </v-col>
+            </v-row>
+          </template>
+          <template v-else>
+            <v-select rounded outlined v-model="residentForm.corridor" :items="corridors" label="Corridor">
+            </v-select>
+            <v-text-field rounded outlined v-model="residentForm.room" label="Room Number"></v-text-field>
+            <v-text-field rounded outlined v-model="residentForm.religion" label="Religion"></v-text-field>
+            <v-text-field rounded outlined v-model="residentForm.civilStatus" label="Civil Status"></v-text-field>
+            <DatePicker v-model="residentForm.birthday" label="Birthday" />
+            <v-text-field rounded outlined v-model="residentForm.stsBracket" label="STS Bracket"></v-text-field>
+          </template>
           <v-textarea rounded outlined v-model="residentForm.homeAddress" label="Home Address"></v-textarea>
           <v-textarea rounded outlined v-model="residentForm.mmAddress" label="Metro Manila Address"></v-textarea>
           <v-textarea rounded outlined v-model="residentForm.foodAllergies" label="Food Allergies"></v-textarea>
           <v-textarea rounded outlined v-model="residentForm.medicineAllergies" label="Medicine Allergies"></v-textarea>
-          <v-textarea rounded outlined v-model="residentForm.diagnosedHealthCondition" label="Diagnosed Health Condition"></v-textarea>
+          <v-textarea rounded outlined v-model="residentForm.diagnosedHealthCondition"
+            label="Diagnosed Health Condition"></v-textarea>
           <v-textarea rounded outlined v-model="residentForm.recurringSymptoms" label="Recurring Symptoms"></v-textarea>
         </v-container>
       </v-card-text>
@@ -126,7 +126,10 @@
 </template>
 
 <script>
-  import { addResident, updateResident } from '@/utils/ekalayapi'
+  import {
+    addResident,
+    updateResident
+  } from '@/utils/ekalayapi'
 
   const DatePicker = () => import('@/components/general/DatePicker')
   const AvatarUploader = () => import('@/components/database/AvatarUploader')
@@ -155,6 +158,9 @@
     computed: {
       photoWatcher() {
         return this.photo.displayPhoto
+      },
+      isMobileDevice() {
+        return (typeof this.$windowOrientation !== "undefined") || (this.$userAgent.indexOf('IEMobile') !== -1);
       }
     },
     watch: {
@@ -171,6 +177,39 @@
       }
     },
     data: () => ({
+      corridors: [{
+          text: 'Basement Boys',
+          value: 'BB'
+        },
+        {
+          text: '1st Floor Boys',
+          value: '1FB'
+        },
+        {
+          text: '2nd Floor Boys',
+          value: '2FB'
+        },
+        {
+          text: '3rd Floor Boys',
+          value: '3FB'
+        },
+        {
+          text: 'Centerfold Girls',
+          value: 'CF'
+        },
+        {
+          text: '1st Floor Girls',
+          value: '1FG'
+        },
+        {
+          text: '2nd Floor Girls',
+          value: '2FG'
+        },
+        {
+          text: '3rd Floor Girls',
+          value: '3FG'
+        }
+      ],
       dialog: false,
       residentForm: {
         upid: '',
