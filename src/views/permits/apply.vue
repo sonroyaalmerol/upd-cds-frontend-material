89147b218eefe605dfe11067e7011ae59168d02b
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-11-01 23:00:59 +0800

    final live

--- src/views/permits/apply.vue
+++ src/views/permits/apply.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container-refresh>
     <ApplicationRules />
     <v-card flat>
       <v-tabs v-model="tab" background-color="transparent" grow>
@@ -26,7 +26,7 @@
         </v-tab-item>
       </v-tabs-items>
     </v-card>
-  </v-container>
+  </v-container-refresh>
 </template>
 
 <script>
