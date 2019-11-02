8e4fe38dc67bd5d54249904ac15604e7753d347c
Son Roy Almerol <sonroya.almerol@gmail.com>
2019-11-02 19:37:05 +0800

    build optimizations

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  presets: [ [ "@vue/app", { useBuiltIns: "entry" } ] ]
+  presets: [ ["@vue/app", {
+      useBuiltIns: "entry",
+      "targets": {
+        "browsers": ["last 2 versions", "safari > 8", "not ie < 11"]
+      }
+    }] 
+  ]
 }
--- vue.config.js
+++ vue.config.js
@@ -4,14 +4,40 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  productionSourceMap: false,
   configureWebpack: {
     optimization: {
       splitChunks: {
-        minSize: 10000,
-        maxSize: 200000,
+        chunks: 'all',
+        name: false,
+        cacheGroups: {
+          styles: {
+            name: false,
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true,
+          }
+        },
       }
     },
     // plugins: [new BundleAnalyzerPlugin()]
+  },
+  chainWebpack: config => {
+    config.plugin('friendly-errors').tap(args => {
+      // the actual transformer defined by vue-cli-3
+      const vueCli3Transformer = args[0].additionalTransformers[0];
+      args[0].additionalTransformers = [
+        // use the actual transformer
+        vueCli3Transformer,
+        // add an other transformer that 'empty' the desired error
+        error => {
+          const regexp = /\[mini-css-extract-plugin\]\nConflicting order between:/;
+          if (regexp.test(error.message)) return {};
+          return error;
+        },
+      ];
+      return args;
+    });
   },
   pwa: {
     workboxPluginMode: 'InjectManifest',
